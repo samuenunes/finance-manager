76e47b0976269c644ee7903c87156e3723583574
Samuel <sns@aluno.ueg.br>
2025-01-23 22:20:06 -0300

    feat: add repositories

---
+++ finance/src/main/java/com/leumas/finance/repository/IncomeRepository.java
@@ -0,0 +1,9 @@
+package com.leumas.finance.repository;
+
+import com.leumas.finance.entity.Income;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IncomeRepository extends JpaRepository<Income, Long> {
+}
