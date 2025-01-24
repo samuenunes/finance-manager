a71ec9a95fffed30bf10b0adf7ff97c796ab1f65
Samuel <sns@aluno.ueg.br>
2025-01-23 22:29:30 -0300

    feat: implement services

--- finance/src/main/java/com/leumas/finance/service/ExpenseService.java
+++ finance/src/main/java/com/leumas/finance/service/ExpenseService.java
@@ -1,4 +1,33 @@
 package com.leumas.finance.service;
 
+import com.leumas.finance.entity.Expense;
+import com.leumas.finance.repository.ExpenseRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class ExpenseService {
+
+    private final ExpenseRepository expenseRepository;
+
+    public ExpenseService(ExpenseRepository expenseRepository) {
+        this.expenseRepository = expenseRepository;
+    }
+
+    public List<Expense> findAll() {
+        return expenseRepository.findAll();
+    }
+
+    public Expense save(Expense expense) {
+        return expenseRepository.save(expense);
+    }
+
+    public Expense findById(Long id) {
+        return expenseRepository.findById(id).orElse(null);
+    }
+
+    public void deleteById(Long id) {
+        expenseRepository.deleteById(id);
+    }
 }
--- finance/src/main/java/com/leumas/finance/service/IncomeService.java
+++ finance/src/main/java/com/leumas/finance/service/IncomeService.java
@@ -1,4 +1,32 @@
 package com.leumas.finance.service;
 
+import com.leumas.finance.entity.Income;
+import com.leumas.finance.repository.IncomeRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class IncomeService {
+    private final IncomeRepository incomeRepository;
+
+    public IncomeService(IncomeRepository incomeRepository) {
+        this.incomeRepository = incomeRepository;
+    }
+
+    public List<Income> findAll() {
+        return incomeRepository.findAll();
+    }
+
+    public Income save(Income income) {
+        return incomeRepository.save(income);
+    }
+
+    public Income findById(Long id) {
+        return incomeRepository.findById(id).orElse(null);
+    }
+
+    public void deleteById(Long id) {
+        incomeRepository.deleteById(id);
+    }
 }
